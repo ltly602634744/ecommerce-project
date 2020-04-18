edc1bc1f02fa7a019e7129c6f68e58d60921e6f9
ltly602634744 <yan.liyan1204@gmail.com>
2020-04-18 12:33:09 -0400

    add @CrossOrigin annotation to make the springrest api can access javascript web page

--- src/main/java/com/example/ecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/com/example/ecommerce/dao/ProductCategoryRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.example.ecommerce.entity.ProductCatagory;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productRepository", path = "product-repository")
 public interface ProductCategoryRepository extends JpaRepository<ProductCatagory, Long> {
 
--- src/main/java/com/example/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/example/ecommerce/dao/ProductRepository.java
@@ -3,7 +3,9 @@ package com.example.ecommerce.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.ecommerce.entity.Product;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
